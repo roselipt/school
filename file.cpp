07dcda9463597afc5f72cede99001f3f7f735549
roselipt <outofastem@gmail.com>
2021-09-23 03:20:14 +0000

    One liner to see daily activity record

--- file.cpp
+++ file.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 int main() {
 
+    cout << "What if I literally just add one line.";
     cout << "And if my grandmother had wheels she'd be a wagon.";
     cout << endl;    
     }
