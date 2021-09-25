05de6ca6b842d58d77b3e3bbfcc92d8cd99d9d4c
roselipt <outofastem@gmail.com>
2021-09-25 19:28:19 +0000

    You have to add source file before each commit?

--- file.cpp
+++ file.cpp
@@ -2,11 +2,31 @@
 */
 
 #include <iostream>
+#include <fstream>
+//#include <string>
 
 using namespace std;
-int main() {
 
-    cout << "What if I literally just add one line.";
-    cout << "And if my grandmother had wheels she'd be a wagon.";
-    cout << endl;    
+int main() {
+    char letter;
+    string line;
+
+    try {
+        ifstream inFile("input.txt");
+        if (!inFile.good()) throw string("Failure opening input.txt");
+        inFile >> line;
+        cout << line;        
+        //cout << "Please type a t." << endl;
+        //cin >> letter;
+        //if (letter != 't') throw string("That's not a t!");
     }
+    catch (string message) {
+        cout << message << endl;
+        exit(1);
+    }
+
+    cout << "You entered " << letter << endl;
+
+    cout << "And if my grandmother had wheels she'd be a wagon" << endl;
+    return(0);  
+    } // close main
