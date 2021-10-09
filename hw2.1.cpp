0dc5a00abcaea31d2cd7e7c1b503151ae9453aee
roselipt <outofastem@gmail.com>
2021-10-09 16:38:04 +0000

    Reviewing operators and strings.

--- hw2.1.cpp
+++ hw2.1.cpp
@@ -16,7 +16,11 @@ int main() {
     cout << "x+=y likewise will perform its calculation before using to we'll expect 17 : " << x << "\n";
     cout << "The interesting I learned there was that the += operation could not be used inline with cout, \n";
     cout << "but as soon as I placed it on its own line with a semicolon it worked as expected. \n";
-    
+    cout << endl;
+    cout << "Knowing that I'll try x%=y on two lines, expecting 17 %= 10 to place 7 in x.\n";
+    x%=y;
+    cout << "Here tis " << x << "\n";
+
     cout << "aimghwsbaw.\n";
 
 }
