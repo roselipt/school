2d725b1b7b369973dfbc9963206aa51d6f2c12ad
roselipt <outofastem@gmail.com>
2021-10-20 15:09:51 +0000

    enums

--- cpp_paradigms/hw2.2.cpp
+++ cpp_paradigms/hw2.2.cpp
@@ -6,6 +6,19 @@
 
 using namespace std;
 
+enum class Gender {male, female};
+
 int main () {
+
+    string playerOne;
+    Gender sex;
+    playerOne = "Player One ";
+    sex = Gender::female;
+    int age = 47;
+
+    cout << playerOne << "\n";
+    cout << "age: " << age << "\n";
+    //  Following enum will produces errors. Must come back to overload << operator for Gender
+    //cout << "sex: " << sex "\n";
     cout << "Here tis.\n";
 }
