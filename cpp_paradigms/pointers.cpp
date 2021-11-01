0ceef468a9860f7f0b5a2a9a248c065beb7a6bc9
tombudny <outofastem@gmail.com>
2021-11-01 08:35:00 -0400

    More practice

--- cpp_paradigms/pointers.cpp
+++ cpp_paradigms/pointers.cpp
@@ -7,15 +7,26 @@
 #include <iostream>
 using namespace std;
 
+void show(int l, int * p) {
+    for (int i = 0; i < l; i++) {
+        cout << *p << " ";
+        p++;
+    }
+}
+
 int main () {
     int length = 10;
     int a[length];
 
     // Show intial values in array
+    cout << "First we see values in array from main: ";
     for (int i = 0; i != length; i++) {
         cout << a[i] << " ";
     }
     cout << "\n";
+
+    //  New we see the same values from function show, taking the array as pointer only
+    show(length, a);
 
     //  Set values in array to integer equal to index
     for (int i = 0; i != length; i++) {
